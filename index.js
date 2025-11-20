0dc28fe56ab525f4965d7ec6f05e5681c36428bd
D4IRY-M1LK09 <itsmetamim1@outlook.com>
2025-11-20 06:42:40 +0600

    Create index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+module.exports = {
+  hello(name = "Jan") {
+    return `Hello ${name} ❤️ From Tamim's Library`;
+  },
+
+  info() {
+    return "This library is created by Tamim (D4IRY-M1LK09)";
+  }
+};
